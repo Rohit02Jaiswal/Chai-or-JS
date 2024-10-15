be548ae475ea633be48cf15b8341255183292f52
Rohit jaiswal <114230762+Rohit02Jaiswal@users.noreply.github.com>
2024-10-15 17:08:56 +0000

    call stack

--- basicJS/function.js
+++ basicJS/function.js
@@ -21,3 +21,41 @@ function packNums (...num){  // rest operator
     return num;
 }
 // console.log(packNums(200,300,500)); // Output : [200,300,500]
+
+// Error function and this keyword -----------------------------------
+const user = {
+    username: "Rohit",
+    price: 999,
+
+    welcomeMessg: function (){
+        console.log(`${this.username}, welcome to website`);
+    }
+}
+
+// user.welcomeMessg()
+
+// function chai(){
+//     console.log(this);
+// }
+// chai();
+
+// const chai = function (){
+//     let username = "jaiswal";
+//     console.log(this.username);
+// }
+
+const chai = () => {
+    let username = "jaiswal";
+    console.log(this.username);
+}
+// chai();
+
+//  Arrow function ----------------------------
+const addTwo = (num1, num2) => {
+    return num1 + num2;
+}
+// console.log(addTwo(5,5));
+
+// Implicit return  (mostly used in React)
+const addNum = (num1, num2) => num1 + num2;
+const addDigit = (num1, num2) => (num1 + num2);
