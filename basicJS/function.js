34faeba6ee327faefcacc10e7cdeb6dc54c41d7a
Rohit jaiswal <114230762+Rohit02Jaiswal@users.noreply.github.com>
2024-10-17 15:50:00 +0000

    changes testing

--- basicJS/function.js
+++ basicJS/function.js
@@ -4,20 +4,20 @@
 // let add = addTwoNums(5, 6);
 // console.log(add);   // It returns undefined
 
-function addTowInteger (num1, num2){
-    return (num1+num2);
+function addTowInteger(num1, num2) {
+    return (num1 + num2);
 }
-let sum = addTowInteger(5,5);
+let sum = addTowInteger(5, 5);
 // console.log(sum);  // Output: 10
 
-function loginUserMessage (username){
+function loginUserMessage(username) {
     return `${username} just logged in`;
 }
 // console.log(loginUserMessage("Rohit"));
 // console.log(loginUserMessage());  // Output: Undefined
 
 // Packing multiple arguments in a single paramater --------------------
-function packNums (...num){  // rest operator 
+function packNums(...num) {  // rest operator 
     return num;
 }
 // console.log(packNums(200,300,500)); // Output : [200,300,500]
@@ -27,7 +27,7 @@ const user = {
     username: "Rohit",
     price: 999,
 
-    welcomeMessg: function (){
+    welcomeMessg: function () {
         console.log(`${this.username}, welcome to website`);
     }
 }
@@ -59,3 +59,21 @@ const addTwo = (num1, num2) => {
 // Implicit return  (mostly used in React)
 const addNum = (num1, num2) => num1 + num2;
 const addDigit = (num1, num2) => (num1 + num2);
+/*
+How does JavaScript excute code + callstack?
+
+JavaScript works on three phases:
+
+Global Execution Context
+Memory phase / Creation phase
+Execution phase
+Phase 1: Global Execution Context The value of this keyword is set to Global Execution Context (which is an empty Object in NODE environment, & window object in browser environment)
+
+Phase 2: Memory creation phase During this phase, JS engine scans the code and assigns the initial values for the variables , functions, etc.. For variables they are 'undefined' and for functions they are set to their function definition
+
+Phase 3: Execution phase After the creation phase completed, the execution phase begins where actual values of the variables are set. And when the functions are called it creates a new 'Function Execution' context which consists of two phases (they works same as explained above but only for the particular function scope):
+
+Memory creation phase
+Execution phase 📝The return value of the 'Function Execution' context is passed to the Global Execution Context
+*/
+
