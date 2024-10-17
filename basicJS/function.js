3679c221127b1f341848c9b692b2123cca3fb4d7
Rohit jaiswal <114230762+Rohit02Jaiswal@users.noreply.github.com>
2024-10-17 18:41:29 +0000

    loop

--- basicJS/function.js
+++ basicJS/function.js
@@ -77,3 +77,117 @@ Memory creation phase
 Execution phase 📝The return value of the 'Function Execution' context is passed to the Global Execution Context
 */
 
+// Switch Case statement ----------------------------------------------------
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 4;
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("No match");
+//         break;
+// }
+
+/*
+falsy value:  false, 0, -0, bigInt 0n, "", null, undefined, NaN
+truthy value:  all value except falsy value- eg: "0", 'false', " ", [], {}, function(){}
+*/
+
+// Nullish coalescing operator (??): null undefined
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+// console.log(val1);
+
+// Loops/Iterations --------------------------------------------
+// for loop
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+// break, continue
+
+//  while & do while loop
+let index = 0;
+while (index<=10) {
+    // console.log(`Value of index is ${index}`);
+    index =  index + 2;
+}
+
+let myArray = ['Rohit', 'Sakshi', 'Patna'];
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`Value of array is ${myArray[arr]}`);
+    arr += 1;
+}
+
+let score = 8;
+do {
+    // console.log(`score is ${score}`);
+    score++;
+} while (score<10);
+
+
+// for of loop (Array specific loop) -----------
+const forOfArr = [1, 2, 3, 4, 5];
+for (const val of forOfArr) {
+    // console.log(val);
+}
+
+// Maps ------------------------
+const map = new Map();
+map.set('IN', "India");
+map.set('USA', "United States of America");
+map.set('IN', "India");  // Map avoid duplicate values
+// console.log(map);
+
+for (const mp of map) {  // output will be in the form of array
+    // console.log(mp);
+}
+
+for (const [key, value] of map) {
+    // console.log(`${key} :-  ${value}`);
+}
+
+// object is not iterable using for of loop
+// Map is not iterable using for in loop
+const Obj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swft: "Swift by apple"
+}
+
+// for in loop ------------
+for (const key in Obj){
+    // console.log(`${key} is shortcut for ${Obj[key]}`);
+}
+
+// for each loop ---------------
+const coding = ['JS', 'Java', 'C++', 'Python', 'Go'];
+coding.forEach(itr => {
+    // console.log(itr);
+});
+// forEach loop kux return nhi krta hai.
+
+// Filter Map and reduce -------------
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+const newNums = myNums.filter((num) => num > 4);
+// console.log(newNums);
